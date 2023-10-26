6d443050cc1ff9c860430b77fa733f45e3a005d3
Gerald <gerald.mwang@gmail.com>
2023-10-25 22:50:25 -0600

    moved input data to input.js.

--- play.js
+++ play.js
@@ -1,28 +1,7 @@
-const net = require("net");
-
-// create the server
-const config = {
-  port: 50541, // PORT number here
-  host: "165.227.47.243" // IP address here,
-};
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection(config);
-
-  // interpret incoming data as text, sets the encoding of the connection
-  conn.setEncoding("utf8");
-
-  // prints this when a successful connection is made.
-  conn.on("connect", () => {
-    console.log("connected to server");
-  });
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
